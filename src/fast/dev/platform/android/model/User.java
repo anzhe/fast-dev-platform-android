6c95616052c418d5824579f6fad9c9b7b3da6923
rong.lin <cnlinrong@gmail.com>
2016-03-29 17:35:39 +0800

    代码调整

--- src/fast/dev/platform/android/model/User.java
+++ src/fast/dev/platform/android/model/User.java
@@ -9,23 +9,23 @@ public class User extends Model {
 
 	@Column(name = "Account")
 	public String account;
-	
+
 	@Column(name = "Password")
 	public String password;
-	
+
 	@Column(name = "Role")
 	public int role;// 0为用户, 1为律师
-	
+
 	public User() {
 		super();
 	}
-	
+
 	public User(String account, String password, int role) {
 		super();
-		
+
 		this.account = account;
 		this.password = password;
 		this.role = role;
 	}
-	
+
 }
